eb7f792e74981949843e7557dd1f76c8502a14b6
bariskayhan93 <bariskayhan53@gmail.com>
2021-12-14 14:27:40 +0100

    get users added

--- routes/urls.js
+++ routes/urls.js
@@ -3,6 +3,19 @@ const router = Express.Router();
 const shortid = require('shortid');
 const User = require('../models/User');
 
+
+
+router.get('/users', async (req,res)=>{
+  User.find({},function(err,users){
+    var userMap={};
+
+    users.forEach(function(user){
+      userMap[user._id]=user;
+    })
+    res.send(Array(userMap))
+  })
+})
+
 // Short URL Generator
 router.post('/users', async (req, res) => {
   const {username} = req.body;
