42570c432af2c0c20d9c08e6eaf5fc65998b88da
Baris Kayhan <83955254+bariskayhan93@users.noreply.github.com>
2021-12-16 01:05:49 +0100

    Update todo.js

--- routes/todo.js
+++ routes/todo.js
@@ -6,14 +6,10 @@ var User = require("../models/User");
 router.post('/:_id/exercises', (req, res) => {
   let id = req.params._id;
   let duration = parseInt(req.body.duration);
-  let date =
-    req.body.date === "" || req.body.date === undefined
-      ? new Date().toDateString()
-      : new Date(Date.parse(req.body.date)).toDateString();
-
-  if (date.toDateString() === 'Invalid Date') {
-    return res.send('Invalid Date');
-  }
+  let date = 
+  req.body.date === "" || req.body.date === undefined
+    ? new Date().toDateString()
+    : new Date(Date.parse(req.body.date)).toDateString();
 
   if (req.body.description == '') {
     return res.send('Invalid description');
@@ -41,7 +37,7 @@ router.post('/:_id/exercises', (req, res) => {
       username: user.username,
       description: req.body.description,
       duration: parseInt(req.body.duration),
-      date: date.toDateString(),
+      date: date,
       _id: user._id
     });
 
